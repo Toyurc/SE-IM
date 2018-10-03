8eaf33cf5853215724f8aa4e051c885483bd7412
Adebayo-Ige <adebayoige.toyosi@gmail.com>
2018-10-03 09:22:54 +0100

    initial commit

---
+++ app/src/main/java/com/example/adeba/se_im/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.adeba.se_im;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
